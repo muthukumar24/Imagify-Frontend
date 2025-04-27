7804e9aa02e007fbb368d28afc5459e67cc3497f
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-27 15:45:08 +0530

    UI Development

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter as Router } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
+
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </Router>
+);
