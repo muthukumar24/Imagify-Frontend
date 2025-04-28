d0bb9b8c1c8b6cd218a01e104f6412eecebdc941
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-28 23:25:56 +0530

    UI development in Progress

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Result = () => {
   return (
-    <div>Result</div>
-  )
-}
+    <form className="flex flex-col justify-center items-center min-h-[90vh]">
+      <div>
+        <div className="relative">
+          <img
+            src={assets.sample_img_1}
+            alt="image"
+            className="w-100 h-100 max-w-sm rounded"
+          />
+          <span
+            className="absolute bottom-0 left-0 h-1
+        bg-blue-500 w-full transition-all duration-[10s]"
+          />
+        </div>
 
-export default Result
+        <p>Loading...</p>
+      </div>
+
+      <div
+        className="flex w-full max-w-xl bg-neutral-500 text-white
+      text-sm p-0.5 mt-10 rounded-full"
+      >
+        <input
+          type="text"
+          placeholder="Describe what you want to generate"
+          className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20
+          placholder-color"
+        />
+        <button
+          type="submit"
+          className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full cursor-pointer"
+        >
+          Generate
+        </button>
+      </div>
+
+      <div className="flex gap-2 flex-wrap justify-center text-white
+        text-sm p-0.5 mt-10 rounded-full">
+        <p className="bg-transparent border border-zinc-900
+        text-black px-8 py-3 rounded-full cursor-pointer">
+          Generate Another
+        </p>
+        <a href="" download className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer">Download</a>
+      </div>
+    </form>
+  );
+};
+
+export default Result;
