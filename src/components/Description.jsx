ee9f459fe7b7a0e7707779f8455acde4c72f519c
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-28 21:47:26 +0530

    UI development in Progress

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -7,18 +7,18 @@ const Description = () => {
         <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
         <p className='text-gray-500 mb-8'>Turn Your Imagination into Visuals</p>
 
-        <div>
+        <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center'>
           <img src={assets.sample_img_1} alt="" className='w-80 xl:w-96 rounded-lg ' />
           <div>
-            <h2>Intorducing the AI Powered Text to Image Generator</h2>
-            <p>
+            <h2 className='text-3xl font-medium max-w-lg mb-4'>Intorducing the AI Powered Text to Image Generator</h2>
+            <p className='text-gray-600 mb-4'>
               Easily bring your ideas to life with our free AI 
               Image Generator. Whether you need stunning visuals or
               unique imagery, our tool transforms your text into 
               eye-catching images with just a few clicks. Imagine it,
               describe it, and watch it come to life instantly.
             </p>
-            <p>
+            <p className='text-gray-600 mb-4'>
               Simply type in a text prompt, and our cutting edge AI
               will generate high quality images in seconds. From product visuals 
               to character designs and portraits, even concepts that dont't yet 
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className='flex flex-col items-center justify-center 
+    my-24 p-6 md:px-28'>
+        
+    </div>
+  )
+}
+
+export default Testimonials
