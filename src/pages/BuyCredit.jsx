de7b9938a413b8c3ab2cbe307e6dca4d7efe5893
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-29 23:34:28 +0530

    UI development in Progress

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets, plans } from '../assets/assets'
+import { AppContext } from '../context/AppContext';
 
 const BuyCredit = () => {
+
+  const { user } = useContext(AppContext);
+
   return (
     <div className='min-h-[80vh] text-center pt-14 mb-10'>
       <button className='border border-gray-400 px-10 py-2
@@ -9,14 +13,24 @@ const BuyCredit = () => {
       <h1 className='text-center text-3xl font-medium mb-6
       sm:mb-6'>Choose The Plan</h1>
 
-      <div>
+      <div className='flex flex-wrap justify-center gap-6 text-left'>
         {
           plans.map((item, index) => (
-            <div key={index}>
-              <img src={assets.logo_icon} alt="icon" />
-              <p>{item.id}</p>
-              <p>{item.desc}</p>
-              <p>₹ {item.price} / {item.credits} credits</p>
+            <div key={index} className='bg-white drop-shadow-sm 
+            rounded-lg py-12 px-8 text-gray-600 
+            hover:scale-105 transition-all duration-500'>
+              <img  width={40} src={assets.logo_icon} alt="icon" />
+              <p className='mt-3 mb-1 font-semibold'>{item.id}</p>
+              <p className='text-sm'>{item.desc}</p>
+              <p className='mt-6'>
+                <span className='text-3xl font-medium'>₹ {item.price}</span> / {item.credits} credits
+              </p>
+              <button className='cursor-pointer w-full bg-gray-800 text-white mt-8
+              text-sm rounded-md py-2.5 min-w-52'>
+                {
+                  user ? "Purchase" : "Get Started"
+                }
+              </button>
             </div>
           ))
         }
