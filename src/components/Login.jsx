9c892f60edb44526dacb02a3fc92a17284d23865
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-29 23:45:38 +0530

    UI development in Progress

---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Login = () => {
+  return (
+    <div className='absolute top-0 left-0 right-0 bottom-0
+    z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+
+
+        <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+            <h1>Sign Up</h1>
+            <p>Welcomeback! Please sign in to continue</p>
+
+            <div>
+                <img src={assets.profile_icon} alt="icon" />
+                <input type="text" placeholder='Full Name' required />
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
