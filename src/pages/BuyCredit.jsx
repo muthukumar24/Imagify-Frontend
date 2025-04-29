a5145ec806362c0fa254e99d9b0d35ccbef97b55
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-29 22:28:43 +0530

    UI development in Progress

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BuyCredit = () => {
   return (
-    <div>BuyCredit</div>
+    <div className='min-h-[80vh] text-center pt-4 mb-10'>
+      <button className='border border-gray-400 px-10 py-2
+      rounded-full mb-6'>Our Plans</button>
+      <h1 className='text-center text-3xl font-medium mb-6
+      sm:mb-6'>Choose The Plan</h1>
+    </div>
   )
 }
 
