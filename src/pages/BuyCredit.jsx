cc2c69bba8f16f4dabfc31027f78fae73ec3036c
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-29 22:34:48 +0530

    UI development in Progress

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { assets, plans } from '../assets/assets'
 
 const BuyCredit = () => {
   return (
-    <div className='min-h-[80vh] text-center pt-4 mb-10'>
+    <div className='min-h-[80vh] text-center pt-14 mb-10'>
       <button className='border border-gray-400 px-10 py-2
       rounded-full mb-6'>Our Plans</button>
       <h1 className='text-center text-3xl font-medium mb-6
       sm:mb-6'>Choose The Plan</h1>
+
+      <div>
+        {
+          plans.map((item, index) => (
+            <div key={index}>
+              <img src={assets.logo_icon} alt="icon" />
+              <p>{item.id}</p>
+              <p>{item.desc}</p>
+              <p>₹ {item.price} / {item.credits} credits</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
