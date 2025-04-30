d28b192acb61fd1926fd13d9f865de3667cc7611
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-30 20:10:38 +0530

    UI development in Progress

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { motion } from "motion/react";
 
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
@@ -12,7 +13,12 @@ const Result = () => {
   }
 
   return (
-    <form onSubmit={onSubmitHandler} 
+    <motion.form 
+    initial={{ opacity: 0.2, y: 100}}
+    transition={{ duration: 1 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true }}
+    onSubmit={onSubmitHandler} 
       className="flex flex-col justify-center items-center min-h-[90vh]">
       <div>
         <div className="relative">
@@ -73,7 +79,7 @@ const Result = () => {
           </a>
         </div>
       )}
-    </form>
+    </motion.form>
   );
 };
 
